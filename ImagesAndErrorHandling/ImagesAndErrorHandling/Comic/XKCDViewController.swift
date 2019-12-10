af38c7e2d2e157a4ea154906d3d247d7cfc2d0be
Kelby Mittan <kelbymittan@pursuit.org>
2019-12-09 20:36:01 -0500

    added textField delegate and a titleLabel

--- ImagesAndErrorHandling/ImagesAndErrorHandling/Comic/XKCDViewController.swift
+++ ImagesAndErrorHandling/ImagesAndErrorHandling/Comic/XKCDViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class XKCDViewController: UIViewController {
 
+    @IBOutlet var titleLabel: UILabel!
     
     @IBOutlet var comicImage: UIImageView!
     @IBOutlet var stepperControl: UIStepper!
@@ -20,6 +21,7 @@ class XKCDViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        textField.delegate = self
         configureStepper()
         comicLoad()
     }
@@ -34,6 +36,9 @@ class XKCDViewController: UIViewController {
                 print("the error was \(error)")
             case .success(let comic):
 
+                DispatchQueue.main.async {
+                    self.titleLabel.text = comic.title
+                }
                 ImageClient.fetchImage(for: comic.img) { [weak self] (result) in
                     switch result {
                     case .failure(let error):
@@ -69,6 +74,44 @@ class XKCDViewController: UIViewController {
     @IBAction func recentButtonPressed(_ sender: UIButton) {
         recent = true
         comicLoad()
+    }
+    
+}
+
+extension XKCDViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        guard let text = textField.text else {
+            return false
+        }
+        let currentText = text + string
+        
+//        let num = ["1","2","3","4","5","6","7","8","9","0"]
+        let numbers = "1234567890"
+        
+        if currentText.count > 4 || !numbers.contains(string) {
+            return false
+        }
+        
+        return true
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        guard let text = textField.text else {
+            return false
+        }
+        if !text.isEmpty {
+            let textAsDouble = Double(text) ?? 0
+            stepperControl.value = textAsDouble
+            comicLoad()
+        }
+        
+        textField.text = ""
+        return true
     }
     
 }
